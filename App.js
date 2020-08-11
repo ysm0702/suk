54d9ea771c2d987774a40f8f02030d66b2b6da97
suk <ysm724@naver.com>
2020-08-11 22:36:40 +0900

    최초?

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+
+const App = () => {
+  return (
+    <ScrollView>
+    
+      <View>
+    
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+		 <Text>윤석민</Text>
+		  <Text>어렵고 열심히 하겠습니다</Text>
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue="You can type in me"
+      />
+    </ScrollView>
+  );
+}
+
+export default App;
